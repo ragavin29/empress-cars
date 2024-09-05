5fbc864fb30e9878e17a03313f795a02ad7ed008
Ragavinehru <ragavin@venzotechnologies.com>
2024-09-05 15:11:00 +0530

    Login functionality added

--- App.js
+++ App.js
@@ -5,6 +5,7 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import Splash from './src/Screens/Splash';
 import Login from './src/Screens/Login';
+import Home from './src/Screens/Home';
 
 const Stack = createNativeStackNavigator();
 function App() {
@@ -13,6 +14,7 @@ function App() {
       <Stack.Navigator initialRouteName='Splash'>
         <Stack.Screen  options={{ headerShown: false }} name="Splash" component={Splash} />
         <Stack.Screen  options={{ headerShown: false }} name="Login" component={Login} />
+        <Stack.Screen  options={{ headerShown: false }} name="Home" component={Home} />
       </Stack.Navigator>
 
      
